f8da952a3d0c619e20a870e5993c5a76d45279ac
Zt-freak <bnkho98@hotmail.com>
2021-01-05 18:18:47 +0100

    added pulse detection

---
+++ Morse/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Morse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pulseButton = new System.Windows.Forms.Button();
+            this.playButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 397);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // pulseButton
+            // 
+            this.pulseButton.Location = new System.Drawing.Point(12, 415);
+            this.pulseButton.Name = "pulseButton";
+            this.pulseButton.Size = new System.Drawing.Size(75, 23);
+            this.pulseButton.TabIndex = 1;
+            this.pulseButton.Text = "Pulse";
+            this.pulseButton.UseVisualStyleBackColor = true;
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(93, 415);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(75, 23);
+            this.playButton.TabIndex = 2;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.pulseButton);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button pulseButton;
+        private System.Windows.Forms.Button playButton;
+    }
+}
+
---
+++ Morse/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+
+namespace Morse
+{
+    public partial class Form1 : Form
+    {
+        private DateTime sequenceTime = DateTime.Now;
+        private Timer endSequenceTimer = new Timer();
+        private string sequence = "";
+        private DateTime pulseStart = DateTime.Now;
+        private DateTime pulseEnd = DateTime.Now;
+        private readonly SoundPlayer beeper = new SoundPlayer(Path.Combine(Environment.CurrentDirectory, @"", "880_Sine_wave.wav"));
+        public Form1()
+        {
+            InitializeComponent();
+            this.pulseButton.MouseDown += new MouseEventHandler(this.pulseButton_Click);
+            this.pulseButton.MouseUp += new MouseEventHandler(this.pulseButton_Release);
+            this.endSequenceTimer.Tick += new EventHandler(this.check_Sequence);
+            this.endSequenceTimer.Interval = 1000; // in miliseconds
+            this.endSequenceTimer.Start();
+        }
+
+        private void pulseButton_Click(object sender, EventArgs e)
+        {
+            this.pulseStart = DateTime.Now;
+            if (this.pulseStart - this.sequenceTime > TimeSpan.FromMilliseconds(300) && this.sequence != "")
+            {
+                Console.WriteLine(this.sequence);
+                this.sequence = "";
+            }
+            beeper.Play();
+        }
+
+        private void pulseButton_Release(object sender, EventArgs e)
+        {
+            beeper.Stop();
+            this.pulseEnd = DateTime.Now;
+            this.sequenceTime = DateTime.Now;
+            TimeSpan beepTime = this.pulseEnd - this.pulseStart;
+            //Console.WriteLine(beepTime);
+            if (beepTime > TimeSpan.FromMilliseconds(150))
+            {
+                this.sequence += "-";
+            }
+            else
+            {
+                this.sequence += ".";
+            }
+        }
+
+        private void check_Sequence(object sender, EventArgs e)
+        {
+            if (DateTime.Now - this.sequenceTime > TimeSpan.FromMilliseconds(1000) && this.sequence != "")
+            {
+                Console.WriteLine(this.sequence);
+                this.sequence = "";
+            }
+        } 
+    }
+}
